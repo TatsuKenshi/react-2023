ee12c1877fa240119bb50ee5b043771d7a6ade73
TatsuKenshi <rockinbg@gmail.com>
2023-10-09 17:17:59 +0200

    adds axios tutorial lessons and examples

--- 05-axios-tutorial/src/examples/2-headers.js
+++ 05-axios-tutorial/src/examples/2-headers.js
@@ -1,23 +1,35 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState("random dad joke");
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      const { data } = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      setJoke(data.joke);
+    } catch (error) {}
   };
+  // fetchDadJoke();
 
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke">{joke}</p>
     </section>
   );
 };
 export default Headers;
+
+// headers are usually placed as the second argument in the request {get request}, right after the url.
+
+// however, if there is a body (post/put/patch requests), headers are the third arg
--- 05-axios-tutorial/src/examples/6-interceptors.js
+++ 05-axios-tutorial/src/examples/6-interceptors.js
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import authFetch from "../axios/interceptors";
 
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch("/react-store-productss");
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
+
+// interceptors are functions axios calls for each request.
+// we can use them to transform a request before it leaves,
+// also we can add custom logic for handling the response
+// interceptors are generally used for complex applications
+// example: authentication
