c9f148548279392b258181b9d185fa6d4ce99646
TatsuKenshi <rockinbg@gmail.com>
2023-10-19 23:59:11 +0200

    adds vite application for the tailwind portfolio, navbar, hero, and skills sections

---
+++ 10-tailwind-portfolio/tailwind-portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+    </>
+  );
+}
+
+export default App;
