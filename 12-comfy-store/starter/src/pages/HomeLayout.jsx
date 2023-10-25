d64700fbcef1d6b99981d257244adcb4d4653e5a
TatsuKenshi <rockinbg@gmail.com>
2023-10-25 17:25:49 +0200

    comfy store basic setup,dependencies, daisyUI, error, login, register pages

---
+++ 12-comfy-store/starter/src/pages/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <nav className="text-4xl text-primary">Comfy</nav>
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
+    </>
+  );
+};
+
+export default HomeLayout;
---
+++ 12-comfy-store/starter/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["winter", "dracula"],
+  },
+};
