d0e5293620bfbc548b771ad715c7d0d52dc053e9
TatsuKenshi <rockinbg@gmail.com>
2023-12-22 22:32:26 +0100

    adds checkout page basic setup

--- 12-comfy-store/starter/src/components/index.jsx
+++ 12-comfy-store/starter/src/components/index.jsx
@@ -14,3 +14,4 @@ export { default as ProductsGrid } from "./ProductsGrid";
 export { default as Loading } from "./Loading";
 export { default as CartItemsList } from "./CartItemsList";
 export { default as CartTotals } from "./CartTotals";
+export { default as CheckoutForm } from "./CheckoutForm";
