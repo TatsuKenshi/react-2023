4fdc2c40c859b82da4b42a8a4c8ab3d1e2c88dbb
TatsuKenshi <rockinbg@gmail.com>
2023-12-24 23:57:22 +0100

    improves checkout form messages

--- 12-comfy-store/starter/src/components/CheckoutForm.jsx
+++ 12-comfy-store/starter/src/components/CheckoutForm.jsx
@@ -1,5 +1,5 @@
 const CheckoutForm = () => {
-  return <h1 className="text-3xl">CheckoutForm</h1>;
+  return <h1 className="text-3xl">Checkout Form</h1>;
 };
 
 export default CheckoutForm;
--- 12-comfy-store/starter/src/pages/Checkout.jsx
+++ 12-comfy-store/starter/src/pages/Checkout.jsx
@@ -4,12 +4,12 @@ import { CheckoutForm, SectionTitle, CartTotals } from "../components";
 const Checkout = () => {
   const cartItems = useSelector((state) => state.cartState.cartTotal);
   if (cartItems.length === 0) {
-    return <SectionTitle text="Your cart is empty" />;
+    return <SectionTitle text="Your cart is empty, fill it" />;
   }
 
   return (
     <>
-      <SectionTitle text="Place your order" />
+      <SectionTitle text="Finalize your order" />
       <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
         <CheckoutForm />
         <CartTotals />
